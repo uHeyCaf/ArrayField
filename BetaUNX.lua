fa0feef7893a9e712fb8a3e5174fb7561c799c08
uHeyCaf <158849925+uHeyCaf@users.noreply.github.com>
2025-06-21 00:25:50 -0400

    Update BetaUNX.lua

--- BetaUNX.lua
+++ BetaUNX.lua
@@ -1,1 +1,4 @@
 -- Hey!
+
+local NotificationSystem = loadstring(game:HttpGet("https://raw.githubusercontent.com/uHeyCaf/ArrayField/refs/heads/main/UNX_NotificationService.lua"))()
+NotificationSystem.Notification("Hey!", "Beta Test Ended!", "OK", "Cancel", false)
